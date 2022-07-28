219523dacb213bb07b5e2b57346e1042812b7a37
John Christopher Jones <john@johnjones.dev>
2022-07-28 11:57:16 -0400

    feat: PUT /items/{item_id}

--- src/hello_world/main.py
+++ src/hello_world/main.py
@@ -1,6 +1,16 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 
 @app.get("/")
 def read_root():
@@ -8,5 +18,10 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
+async def read_item(item_id: int = 1, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
